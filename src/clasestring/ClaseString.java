2a6a5b045530c7c771dc977cb50ae64509a166ec
JCarlosVico <jfervic933@maralboran.es>
2019-04-14 09:33:26 +0200

    reset y commit

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -14,10 +14,10 @@ public class ClaseString {
    
     public static void main(String[] args) {
         
-        String s = "iesmardealboran.com";
-        
-        // Tamaño de la cadena
-        System.out.println("Tamaño: " + s.length());
+        String s = "yo.com";
+        // Hola 
+      
+        System.out.println("Tamaños: " + s.length());
         
         // Obtener el char de una posicion concreta
         System.out.println("char de la posicion 5: " + s.charAt(5));
